399bd2883123b45fcd4255efaa5c16dd2e938db1
Wave Cantery Ambray <wacanam20mail.com>
2024-06-28 01:16:18 +0800

    refactor: Update Header component to include welcome message
    The code changes in `Header.tsx` update the Header component to include a welcome message in the largest book hub in town. The message is wrapped in a `span` element with the `font-bold` class for emphasis. This refactor improves the user experience by providing a more personalized greeting.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       
       <h1 className="sr-only">The largest book hub in town.</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The largest book hub in town.
+        <span className="font-bold">Welcome</span> to the largest <span className="font-bold">book hub</span> in town.
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
