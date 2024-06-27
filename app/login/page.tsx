d43abc1ecb5b22a44ae15ad2d542f23f0f801295
Wave Cantery Ambray <wacanam20mail.com>
2024-06-28 01:17:24 +0800

    refactor: Update login page redirect to /books
    This commit updates the login page to redirect users to the /books page instead of the /protected page after successful authentication. This change aligns with the desired user flow and improves the navigation experience.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,7 +25,7 @@ export default function Login({
       return redirect("/login?message=Could not authenticate user");
     }
 
-    return redirect("/protected");
+    return redirect("/books");
   };
 
   const signUp = async (formData: FormData) => {
@@ -52,10 +52,10 @@ export default function Login({
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 ">
       <Link
         href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+        className="absolute left-8 top-20 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -96,7 +96,7 @@ export default function Login({
         />
         <SubmitButton
           formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          className="bg-green-600 rounded-md px-4 py-2 text-white mb-2"
           pendingText="Signing In..."
         >
           Sign In
